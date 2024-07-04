84d0b5a48769fa3b4544780944b6def52a361aa4
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-07-04 14:48:28 +0100

    Add docstring to concat function

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
+"""
+This module provides functions for string concatenation.
+"""
 
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
     return str1 + str2
