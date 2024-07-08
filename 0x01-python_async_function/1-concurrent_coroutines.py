e4aa81bbbf9380783937650f9fca3b4ac146a7cc
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-07-08 15:12:06 +0100

    Add concurrent coroutine execution function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,27 @@
+#!/usr/bin/env python3
+"""
+This module contains a function for concurrently executing coroutines.
+"""
 from typing import List
 import importlib
 import asyncio
+
 
 wait_random = importlib.import_module('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List:
+    """
+    Waits for a random delay multiple times and
+        returns a sorted list of the delays.
+
+    Args:
+        n (int): The number of times to wait.
+        max_delay (int): The maximum delay in seconds.
+
+    Returns:
+        List: A sorted list of the delays.
+    """
     delay_list = await asyncio.gather(
         *tuple(map(lambda _: wait_random(max_delay), range(n)))
     )
