846f837d459d45d5a28f3b19f8673fb127c0f5a4
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-07-08 15:56:52 +0100

    Add task_wait_n function to execute coroutines concurrently

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+This module contains a function for concurrently executing coroutines.
+"""
+from typing import List
+import importlib
+import asyncio
+
+
+task_wait_random = importlib.import_module('0-basic_async_syntax').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Waits for a random delay multiple times and
+        returns a sorted list of the delays.
+
+    Args:
+        n (int): The number of times to wait.
+        max_delay (int): The maximum delay in seconds.
+
+    Returns:
+        List: A sorted list of the delays.
+    """
+    delay_list = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(delay_list)
