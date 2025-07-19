ffdaccb3d07f1d59d9785c1c94278cf9af9e7b60
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-07-19 02:06:17 +0100

    Refactor chat URLs and views to include search functionality and improve routing structure

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,41 +1,16 @@
-# Create your views here.
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import action
-from django.shortcuts import get_object_or_404
-
+# chats/views.py
+from rest_framework import viewsets, filters
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
-
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        # Return only conversations where the current user is a participant
-        return self.queryset.filter(participants=self.request.user)
-
-    def perform_create(self, serializer):
-        # Automatically add current user to participants if not already included
-        conversation = serializer.save()
-        if self.request.user not in conversation.participants.all():
-            conversation.participants.add(self.request.user)
-
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['participants__first_name', 'participants__email']
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        # Return messages only in conversations the user is part of
-        return self.queryset.filter(conversation__participants=self.request.user)
-
-    def perform_create(self, serializer):
-        conversation = serializer.validated_data.get('conversation')
-        if self.request.user not in conversation.participants.all():
-            raise PermissionError("You are not a participant in this conversation.")
-        serializer.save(sender=self.request.user)
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['sender__email', 'message_body']
