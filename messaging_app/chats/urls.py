b3f898649fa6d987b2699fdc0410f1b32ed53b97
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-07-19 02:36:28 +0100

    Refactor URL routing for conversations and messages in the messaging app

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,15 +3,14 @@ from rest_framework import routers
 from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-# This is what the checker wants to see 👇
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversations')
 
-# Nested router for messages under conversations 👇
+
 conversation_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
 conversation_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
-    path('', include(router.urls)),               # Default routes (for conversations)
-    path('', include(conversation_router.urls)),  # Nested routes (for messages)
-]
+    path('', include(router.urls)), 
+    path('', include(conversation_router.urls)),
+] 
