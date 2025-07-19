390f95eb0d089a291f39b8bdeed035d082854da3
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-07-19 02:27:03 +0100

    modified the views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -17,13 +17,19 @@ class ConversationViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
     serializer_class = MessageSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['content']
 
+    def get_queryset(self):
+        conversation_id = self.kwargs.get('conversation_pk')
+        return Message.objects.filter(conversation__id=conversation_id)
+
     def create(self, request, *args, **kwargs):
+        conversation_id = self.kwargs.get('conversation_pk')
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        message = serializer.save(sender=request.user)
+        conversation = Conversation.objects.get(pk=conversation_id)
+        message = serializer.save(sender=request.user, conversation=conversation)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
