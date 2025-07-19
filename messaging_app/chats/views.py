f7cd1cd571eab8f94c053146bfeb7f141044ee68
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-07-19 02:15:35 +0100

    Update settings, refactor URL routing, and enhance viewsets for conversations and messages

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,16 +1,29 @@
-# chats/views.py
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, status, filters
+from rest_framework.response import Response
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
+from django.contrib.auth.models import User
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['participants__first_name', 'participants__email']
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['id', 'updated_at']  # Optional fields for ordering
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        conversation = serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['sender__email', 'message_body']
+    search_fields = ['content']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        message = serializer.save(sender=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -100,6 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'chats.User'
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
