d9aab53febb9f60999cdaad0e21aa0961ac255cb
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-07-26 16:37:22 +0100

    fix: fix the pagination logic

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -9,6 +9,7 @@ class CustomPagination(PageNumberPagination):
     def get_paginated_response(self, data):
         return Response({
             'count': self.page.paginator.count,
+            'num_pages': self.page.paginator.num_pages,
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
             'results': data
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -143,7 +143,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.CustomPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
