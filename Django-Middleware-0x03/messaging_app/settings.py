3496be10d235a2cf302f55cd9830e951dc7b0401
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-07-26 21:28:29 +0100

    Remove commented custom middleware line from settings.py

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -53,8 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-       # ✅ custom middleware
     'chats.middleware.RequestLoggingMiddleware',
 ]
 
